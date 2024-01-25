51f899e5a8b40adf4b033b955e8d99a96ca07c87
nColli <collinicolas01@gmail.com>
2024-01-25 17:13:17 -0300

    3.7 morgan middleware

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+var morgan = require('morgan')
 
 app.use(express.json())
 
@@ -25,6 +26,8 @@ let persons = [
       "number": "39-23-6423122"
     }
 ]
+
+app.use(morgan('tiny'))
 
 app.get('/api/persons', (request, response) => {
   response.json(persons)
