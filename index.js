a18b978375aede1f664920cb6aad4653fcb714d3
nColli <collinicolas01@gmail.com>
2024-01-25 15:29:25 -0300

    3.2 info

--- index.js
+++ index.js
@@ -26,15 +26,13 @@ let persons = [
     }
 ]
 
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
-
 app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+    response.send(`<p>Phonebook has info for ${persons.length}</p> <p>${new Date()}</p>`)
+})
 
 
 const PORT = 3001
