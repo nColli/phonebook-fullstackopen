b26c273347407b8c00026b4f352d6a541e27ab26
nColli <collinicolas01@gmail.com>
2024-01-29 16:43:25 -0300

    show phones

--- mongo.js
+++ mongo.js
@@ -19,14 +19,28 @@ const personSchema = new mongoose.Schema({
 
 const Person = mongoose.model('Person', personSchema) //constructor
 
-//objeto a añadir
-const person = new Person({
-  name: process.argv[3],
-  number: process.argv[4]
-})
+//console.log(process.argv);
 
-//agrego a db
-person.save().then(result => {
-  console.log('person saved!')
-  mongoose.connection.close()
-})
+if (process.argv.length === 3) {
+    Person.find({}).then(result => {
+        console.log("Phonebook");
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`);
+        })
+        mongoose.connection.close()
+    })
+} else {
+    //objeto a añadir
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4]
+    })
+
+    //agrego a db
+    person.save().then(result => {
+        console.log(`Added ${person.name} number ${person.number} to phonebook`)
+        mongoose.connection.close()
+    })
+}
+
+
