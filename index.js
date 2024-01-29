59f09560bb8330bd7cb6c8469a675fe35f0f8f13
nColli <collinicolas01@gmail.com>
2024-01-29 08:46:23 -0300

    cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 var morgan = require('morgan')
+const cors = require('cors')
 
 app.use(express.json())
+
+app.use(cors())
 
 morgan.token('content', function(req, res) {
     return JSON.stringify(req.body);
