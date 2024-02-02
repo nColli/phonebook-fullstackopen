ce6692c3bc9a5110f5854680926ae2380376dada
nColli <collinicolas01@gmail.com>
2024-02-02 11:21:35 -0300

    fix personSchema

--- index.js
+++ index.js
@@ -47,7 +47,9 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/info', (request, response) => {
-    response.send(`<p>Phonebook has info for ${persons.length}</p> <p>${new Date()}</p>`)
+    Person.find({}).then(persons => {
+        response.send(`<p>Phonebook has info for ${persons.length}</p> <p>${new Date()}</p>`)
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
